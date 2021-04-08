76eda28707dc15557250b91296170214cc66424d
wooshrow <>
2021-04-08 11:30:58 +0200

    Changing EventProducer to produces Messages instead of Events.

--- src/main/java/agents/EventsProducer.java
+++ src/main/java/agents/EventsProducer.java
@@ -2,57 +2,74 @@ package agents;
 
 import java.util.*;
 
-import eu.iv4xr.framework.extensions.occ.Event;
 import eu.iv4xr.framework.mainConcepts.WorldEntity;
 import eu.iv4xr.framework.spatial.Vec3;
+import nl.uu.cs.aplib.multiAgentSupport.ComNode;
+import nl.uu.cs.aplib.multiAgentSupport.Message;
+import nl.uu.cs.aplib.multiAgentSupport.Message.MsgCastType;
 import world.LabEntity;
 
 /**
- * 
- * 
- * @author iswbprasetya
- *
+ * This class interprets a Lab Recruit states and can produces events signaling
+ * certain interesting things it observes in the state. Events will be represented
+ * as instances of the class nl.uu.cs.aplib.multiAgentSupport.Message. Generated
+ * instances will be placed in a list which can be inspected. They will also be
+ * sent to a ComNode, if one is provided.
  */
 public class EventsProducer {
 	
-	/**
+    public static final String LabRecruits = "LabRecruits" ;
+    
+    static Message mkLabEvent(int priority, String eventName) { 
+        return new Message(LabRecruits,priority,MsgCastType.BROADCAST,"",eventName) ;
+    }
+    
+    public static final String OuchEventName = "Ouch" ;
+    public static final String OpeningADoorEventName = "Opening a door" ;
+    public static final String GetPointEventName = "Geting some point" ;
+    public static final String LevelCompletedEventName = "Level is completed" ; 
+    
+    /**
 	 * Crossing fire ... it hurts.
 	 */
-	public static class OuchEvent extends Event {
-		
-		public OuchEvent() { super("Ouch") ; }
-	}
+	public static Message ouchEvent() { return mkLabEvent(2,OuchEventName) ; }
+	public static boolean isOuchEvent(Message m) { return m.getMsgName().equals(OuchEventName) ; }
 	
 	/**
 	 * This event is generated when a door that was observed as closed, is now observed as open.
 	 */
-	public static class OpeningADoorEvent extends Event {
-		
-		public OpeningADoorEvent() { super("Opening a door") ; }
-	}
-	
+	public static Message openingADoorEvent() { return mkLabEvent(1,OpeningADoorEventName) ; }
+	public static boolean isOpeningADoorEvent(Message m) { return m.getMsgName().equals(OpeningADoorEventName) ; }
+    
 	/**
 	 * This event is generated whenever the agent receives new points.
 	 */
-	public static class GetPointEvent extends Event {
-		
-		public GetPointEvent() { super("Geting some point") ; }
-	}
+	public static Message getPointEvent(){ return mkLabEvent(0,GetPointEventName) ; }
+    public static boolean isGetPointEvent(Message m) { return m.getMsgName().equals(GetPointEventName) ; }
+
+    public static Message levelCompletedEvent(){ return mkLabEvent(2,LevelCompletedEventName) ; }
+	public static boolean isLevelCompletedEvent(Message m) { return m.getMsgName().equals(LevelCompletedEventName) ; }
+
+
+	/**
+	 * Produced events will be placed here.
+	 */
+	public List<Message> currentEvents = new LinkedList<>() ;
 	
-	public static class LevelCompletedEvent extends Event {
-		public LevelCompletedEvent() { super("Level is completed") ; }
-	}
-	
-	public static String OuchEventName = new OuchEvent().name ;
-	public static String GetPointEventName = new GetPointEvent().name ;
-	public static String OpeningADoorEventName = new OpeningADoorEvent().name ;
-	public static String LevelCompletedEventName = new LevelCompletedEvent().name ;
-	
-	public List<Event> currentEvents = new LinkedList<>() ;
+	/**
+	 * If not null, produced events will also be sent to this communication node.
+	 * Agents registering to this node will then automatically receive the events.
+	 */
+	public ComNode communicationNode ;
 	
 	LabRecruitsTestAgent agent ;
 	
 	public EventsProducer() { }
+	
+	public EventsProducer attachComNode(ComNode comnode) { 
+	    communicationNode = comnode ; 
+	    return this ; 
+	}
 	
 	public EventsProducer attachTestAgent(LabRecruitsTestAgent agent) {
 		this.agent = agent ; return this ;
@@ -60,20 +77,24 @@ public class EventsProducer {
 	
 	public void generateCurrentEvents() {
 		currentEvents.clear() ;
-		if(agent.getState().worldmodel.healthLost>0) currentEvents.add(new OuchEvent()) ;
-		if(agent.getState().worldmodel.scoreGained>0) currentEvents.add(new GetPointEvent()) ;
+		if(agent.getState().worldmodel.healthLost>0) currentEvents.add(ouchEvent()) ;
+		if(agent.getState().worldmodel.scoreGained>0) currentEvents.add(getPointEvent()) ;
 		List<WorldEntity> z = agent.getState().changedEntities ;
 		Vec3 p = agent.getState().worldmodel.position ;
 		
 		if(z!=null && z.stream().anyMatch(entity -> entity.type == LabEntity.DOOR && entity.getBooleanProperty("isOpen"))) {
-			currentEvents.add(new OpeningADoorEvent()) ;
+			currentEvents.add(openingADoorEvent()) ;
 		}	
 		
 		if(agent.getState().worldmodel.elements.values().stream().anyMatch(entity -> 
 		           entity.id.equals("levelEnd") 
 		           && Vec3.dist(entity.position,p) <= 1.5)) {
 			//System.out.println("@@@@@@ LEVEL END") ;
-			currentEvents.add(new LevelCompletedEvent()) ;
+			currentEvents.add(levelCompletedEvent()) ;
+		}
+		// send the events to the comNode, if one is provided:
+		if(communicationNode != null) {
+		    for(Message m : currentEvents) communicationNode.send(m) ;
 		}
 	}
 	
